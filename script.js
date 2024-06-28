b18b88c29023e247e73b6811f97f4645103d99bd
Akram Ahmed Khan <68415682+akram02-sde@users.noreply.github.com>
2024-06-28 09:44:38 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,17 @@
 function createTable() {
     //Write your code here
-  
+	const table = document.getElementById("myTable")
+	table.innerHTML = ""
+	const rows = prompt("Input number of rows");
+	const cols = prompt("Input number of columns");
+
+	for (let row = 0; row < rows; row++) {
+		const tr = document.createElement("tr");
+		for (let col = 0; col < cols; col++) {
+			const td = document.createElement("td")
+			td.innerText = `Row-${row} Column-${col}`
+			tr.append(td);
+		}
+		table.append(tr)
+	}
 }
